cf7dcff09db90284cfa9b640f14e07a7d436e28d
unknown <luiscarlos81@gmail.com>
2024-02-04 21:18:29 -0600

    Contrador

---
+++ raylwaygithub/src/main/java/dev/luismartinez/raylwaygithub/RaylwaygithubApplication.java
@@ -0,0 +1,13 @@
+package dev.luismartinez.raylwaygithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaylwaygithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaylwaygithubApplication.class, args);
+	}
+
+}
